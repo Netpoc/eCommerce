01a36a9f25c9b03a41f67f372d2d8e556e7e2897
Netpoc <ayenotopeter@hotmail.com>
2023-01-16 09:59:42 +0000

    Header and Footer with Project initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app-core/common/header/header.component';
+import { FooterComponent } from './app-core/common/footer/footer.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { ProductsListingComponent } from './components/marketplace/products-listing/products-listing.component';
+import { ProductDetailComponent } from './components/marketplace/product-detail/product-detail.component';
+import { CartComponent } from './components/marketplace/cart/cart.component';
+import { CheckoutComponent } from './components/marketplace/checkout/checkout.component';
+import { ForgottenPasswordComponent } from './components/auth/forgotten-password/forgotten-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProductsListingComponent,
+    ProductDetailComponent,
+    CartComponent,
+    CheckoutComponent,
+    ForgottenPasswordComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/marketplace/products-listing/products-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-listing',
+  templateUrl: './products-listing.component.html',
+  styleUrls: ['./products-listing.component.scss']
+})
+export class ProductsListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
